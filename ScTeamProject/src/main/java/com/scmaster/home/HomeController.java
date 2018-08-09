240574928faaefd77a32a69e8a8fb3e4a2a195c1
ssg8837 <ssg8837@gmail.com>
2018-08-09 18:52:02 +0900

    startting

--- ScTeamProject/src/main/java/com/scmaster/home/HomeController.java
+++ ScTeamProject/src/main/java/com/scmaster/home/HomeController.java
@@ -1,11 +1,11 @@
 package com.scmaster.home;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
+import javax.servlet.http.HttpSession;
 
+import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,20 +19,30 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
+	@Autowired SqlSession sqlSession;
+	@Autowired HttpSession httpSession;
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+	public String home(Model model) {
+		return "home";
+	}
+	@RequestMapping(value = "/openNewAccount", method = RequestMethod.GET)
+	public String openNewAccount(Model model) {
+		return "newAccount";
+	}
+	@RequestMapping(value = "/insertNewAccount", method = RequestMethod.GET)
+	public String insertNewAccount(Model model) {
+		return "insertNewAccount";
+	}
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login(Model model) {
+		return "home";
+	}
+	@RequestMapping(value = "/logout", method = RequestMethod.GET)
+	public String logout() {
+		httpSession.invalidate();
 		return "home";
 	}
 	
