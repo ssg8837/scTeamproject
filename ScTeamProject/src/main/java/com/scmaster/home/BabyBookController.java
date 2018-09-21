241043dd84d1668c00f3c95c9fe4aa97ba59be28
leesa58 <leesa_58@naver.com>
2018-09-21 16:58:22 +0900

    444

--- ScTeamProject/src/main/java/com/scmaster/home/BabyBookController.java
+++ ScTeamProject/src/main/java/com/scmaster/home/BabyBookController.java
@@ -1,18 +1,10 @@
 package com.scmaster.home;
 
-import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.apache.ibatis.session.SqlSession;
@@ -27,24 +19,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.scmaster.mapper.BabyBookMapper;
 import com.scmaster.mapper.MainMapper;
-import com.scmaster.vo.BS_Alarm;
-import com.scmaster.vo.BS_Baby;
+import com.scmaster.vo.BS_User;
 import com.scmaster.vo.BabyBook;
-import com.scmaster.vo.Cal_Event;
 
 
 @Controller
 public class BabyBookController {
 	
-	@Autowired
-	SqlSession sqlSession;
+	@Autowired SqlSession sqlSession;
+	@Autowired HttpSession httpSession;
 	
 	private static final String UPLOADPATH = "C://FileRepo";
 	
@@ -64,8 +52,13 @@ public class BabyBookController {
 		BabyBookMapper mapper = sqlSession.getMapper(BabyBookMapper.class);
 		List<BabyBook> list = mapper.selectListByMonth(smonth);
 		
-		
 		model.addAttribute("list", list);
+		
+		//프로필사진 불러오기
+		Object loginNo=httpSession.getAttribute("loginNo");
+		MainMapper mapperM=sqlSession.getMapper(MainMapper.class);
+		BS_User user=mapperM.myAccount((Integer)loginNo);
+		model.addAttribute("user",user);
 		
 		return "babyBook";
 	}
